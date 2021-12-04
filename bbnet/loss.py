f57bd38a271cbaab44280ed16cf05f9acad9a684
John Carlo Roberto <john.carlo9517@gmail.com>
2021-12-04 14:14:47 +0800

    final loss.py

--- bbnet/loss.py
+++ bbnet/loss.py
@@ -1,3 +1,9 @@
+
+"""
+A loss function measures how far our predictions are to the target output (how good/bad our 
+predictions are). The adjustments we will do to the parameters(weights and biases) of our neural
+network will be based from this.
+""" 
 import numpy as np
 
 from numpy import ndarray
@@ -12,7 +18,8 @@ class Loss:
 
 class TSE(Loss):
   """
-  Total Squared Error
+  TSE means Total Squared Error, meaning we will square the differences between our predictions
+  and our target values then sum them all up
   """
   def loss(self, predictions: ndarray, targets: ndarray) -> float:
     return np.sum((predictions - targets) ** 2)
