778770b6cb5db6116824ddcb4d92ef08692dfd97
John Carlo Roberto <john.carlo9517@gmail.com>
2021-12-04 15:38:31 +0800

    final draft optimizers.py

--- bbnet/optimizers.py
+++ bbnet/optimizers.py
@@ -1,4 +1,9 @@
-
+"""
+Optimizers are the ones who are going to adjust the
+parameters of the neural network according to the
+gradients computed during backpropagation. Different
+optimizers adjust the parameters in different ways
+"""
 from bbnet.nn import NeuralNet
 
 class Optimizer:
@@ -11,6 +16,7 @@ class GD(Optimizer):
   
   def step(self, net: NeuralNet):
     """
+    A Gradient Descent Step is equal to the Negative Gradient multiplied to the Learning Rate:
     step = -gradient * lr
     """
     for layer in net.layers:
